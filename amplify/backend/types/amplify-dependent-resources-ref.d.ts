4dbbc16d4fc1a6846f7b8ed073df49e6aba0acea
Sayan <sayanmajumdar4444@gmail.com>
2026-01-23 00:51:51 +0530

    AUTHENTICATION (COGNITO + AMPLIFY)

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ampnxtapp11d461555": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
